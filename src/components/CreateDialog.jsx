0dc9ba24c1f0078bbfa5090b943758f35d0b0d87
Rinidh <rinidhdhokia@gmail.com>
2025-09-28 14:29:03 +0300

    Manage form inputs's state in CreateDialog and submit values to create new note/task. Enable closing CreateDialog by clicking outside or pressing ESC key

--- src/components/CreateDialog.jsx
+++ src/components/CreateDialog.jsx
@@ -1,18 +1,39 @@
 import React, { useEffect, useRef, useState } from 'react';
 import '../style/CreateDialog.css';
 
-export const CreateDialog = () => {
+export const CreateDialog = ({ onCreateNoteTask }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [newNoteTask, setNewNoteTask] = useState({ type: 'note', content: '' }); //newNoteTask means new note or new task
   const dialogRef = useRef(null);
 
   useEffect(() => {
     const dialog = dialogRef.current;
     if (!dialog) return;
 
-    // dialog.showModal()
-    if (isOpen) dialog.showModal();
-    else dialog.close();
+    dialog.showModal();
+    if (isOpen) {
+      dialog.showModal();
+      setNewNoteTask({ type: 'note', content: '' });
+    } else dialog.close();
   }, [isOpen]);
+
+  useEffect(() => {
+    const dialog = dialogRef.current;
+    if (!dialog) return;
+
+    const handleOutsideClick = (e) => {
+      if (e.target === dialog) setIsOpen(false); // don't dialog.close() from here as it would not update the state
+    };
+    const handleDialogClose = () => setIsOpen(false);
+
+    document.addEventListener('click', handleOutsideClick);
+    dialog.addEventListener('close', handleDialogClose);
+
+    return () => {
+      dialog.removeEventListener('close', handleDialogClose);
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, []);
 
   return (
     <div>
@@ -24,22 +45,52 @@ export const CreateDialog = () => {
       </button>
       <dialog ref={dialogRef}>
         <button className="lato-black close-btn">&times;</button>
-        <form>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            onCreateNoteTask(newNoteTask);
+            setIsOpen(false);
+          }}
+        >
           <fieldset>
             <legend>Create a Note or a Task</legend>
             <div className="type-select">
               <label htmlFor="note">
-                <input type="radio" name="type" value="Note" id="note" />
+                <input
+                  type="radio"
+                  name="type"
+                  value="Note"
+                  id="note"
+                  onChange={() =>
+                    setNewNoteTask((v) => ({ ...v, type: 'note' }))
+                  }
+                  checked={newNoteTask.type === 'note' ? true : false}
+                />
                 Note
               </label>
               <label htmlFor="task">
-                <input type="radio" name="type" value="Task" id="task" />
+                <input
+                  type="radio"
+                  name="type"
+                  value="Task"
+                  id="task"
+                  onChange={() =>
+                    setNewNoteTask((v) => ({ ...v, type: 'task' }))
+                  }
+                  checked={newNoteTask.type === 'task' ? true : false}
+                />
                 Task
               </label>
             </div>
           </fieldset>
           <fieldset>
-            <textarea name="content"></textarea>
+            <textarea
+              name="content"
+              value={newNoteTask.content}
+              onChange={function (e) {
+                setNewNoteTask((v) => ({ ...v, content: e.target.value }));
+              }}
+            ></textarea>
           </fieldset>
 
           <button>Create</button>
